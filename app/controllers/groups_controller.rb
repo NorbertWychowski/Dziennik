0c40b7ebea96a6e29416d78c00d8d25e4eba4223
NorbertWychowski <norbertwychowski@gmail.com>
2018-03-09 15:08:39 +0100

    Poprawki

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,27 +1,27 @@
 class GroupsController < ApplicationController
-	def index
-		@groups = Group.all
-	end
+  def index
+    @groups = Group.all
+  end
 
-	def show
-		@group = Group.find(params[:id])
-	end
+  def show
+    @group = Group.find(params[:id])
+  end
 
-	def new
-		@group = Group.new
-	end
+  def new
+    @group = Group.new
+  end
 
-	def create
-		@group = Group.new(group_params)
+  def create
+    @group = Group.new(group_params)
 
-		if(@group.save)
-			redirect_to @group
-		else
-			render 'new'
-		end
-	end
+    if (@group.save)
+      redirect_to @group
+    else
+      render 'new'
+    end
+  end
 
-	private def group_params
-		params.require(:group).permit(:year, :groupID)
-	end
+  private def group_params
+    params.require(:group).permit(:year, :groupID)
+  end
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,27 +1,28 @@
 class StudentsController < ApplicationController
-	def index
-		@students = Student.all
-	end
+  def index
+    @students = Student.all
+  end
 
-	def show
-		@student = Student.find(params[:id])
-	end
+  def show
+    @student = Student.find(params[:id])
+    @grades = @student.grades.all;
+  end
 
-	def new
-		@student = Student.new
-	end
+  def new
+    @student = Student.new
+  end
 
-	def create
-		@group = Group.find(params[:group_id])
-		@student = @group.students.create(student_params)
+  def create
+    @group = Group.find(params[:group_id])
+    @student = @group.students.create(student_params)
 
-		redirect_to group_path(@group)
-	end
+    redirect_to group_path(@group)
+  end
 
-	def destroy
-	end
+  def destroy
+  end
 
-	private def student_params
-		params.require(:student).permit(:firstName, :lastName)
-	end
+  private def student_params
+    params.require(:student).permit(:firstName, :lastName)
+  end
 end
