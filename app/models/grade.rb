42aaddf4e18dec9b7580e387c2a4c9a85e24f49f
NorbertWychowski <norbertwychowski@gmail.com>
2018-03-30 12:40:09 +0200

    v126.5437.456 MK 3

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -1,9 +1,22 @@
 class Grade < ApplicationRecord
-  def self.gradeValues(grade)
-    grades = {"1" => 1.0, "-2" => 1.75, "2" => 2.0, "+2" => 2.5, "-3" => 2.75, "3" => 3, "+3" => 3.5,
-                     "-4" => 3.75, "4" => 4, "+4" => 4.5, "-5" => 4.75, "5" => 5, "+5" => 5.5, "6" => 6}
-    grades[grade]
+  @grades = {"1" => 1.0, "-2" => 1.75, "2" => 2.0, "+2" => 2.5, "-3" => 2.75, "3" => 3, "+3" => 3.5,
+             "-4" => 3.75, "4" => 4, "+4" => 4.5, "-5" => 4.75, "5" => 5, "+5" => 5.5, "6" => 6}
+  @grade_info = %w(Kartkówka Sprawdzian)
+
+  def self.grade_values(grade)
+    @grades[grade]
   end
+
+  def self.get_grades
+    @grades
+  end
+
+  def self.get_grade_info
+    @grade_info
+  end
+
+  validates :grade, inclusion: {in: @grades.keys}
+  validates :info, inclusion: {in: @grade_info}
 
   belongs_to :subject
   belongs_to :student
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,10 @@
 class Note < ApplicationRecord
+  validate :note_type_exists
+
+  def note_type_exists
+    return false if NoteType.find_by_id(self.note_type).nil?
+  end
+
   belongs_to :user
   belongs_to :student
   belongs_to :note_type
